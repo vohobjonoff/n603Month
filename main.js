952c8832f940c758f435c74c5932f9e0aacb0b2c
kingbookk <kingbooktashkent@gmail.com>
2022-08-23 12:59:27 +0500

    for loop

---
+++ main.js
@@ -0,0 +1,9 @@
+var students = ["Usmon", "Habibullo", "Izzat", "Asomiddin", "Abdulloh", "Asomiddin", "Abdulloh", "Asomiddin", "Abdulloh"];
+
+// console.log(students);
+// console.log(students.length);
+// console.log(students[0]);
+
+// for (var i = 0; i < students.length; i++){
+//     console.log( `Hurmatli ${students[i]} imtihondan successfuly pass `);
+// }
